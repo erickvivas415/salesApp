5640b9835e6191b6cb9d48a5d7025feb7c8750e2
Erick Vivas <88099754+erickvivas415@users.noreply.github.com>
2022-09-21 17:22:05 -0700

    templates

--- salesApp/objectionApp/views.py
+++ salesApp/objectionApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,redirect, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
